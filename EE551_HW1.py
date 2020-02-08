db21a473d557ff3d0691dd367c1533b956e9da22
i5331234 <60754752+i5331234@users.noreply.github.com>
2020-02-08 16:00:07 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -14,15 +14,20 @@ def numbers_and_strings():
     x = "EE551"
 
     # Assign a string "Stevens" to a variable y
-    y
+    y = "Stevens"
 
     # Repeat variable y 5 times
+    y * 5
 
     # What is the length of z?
+    len(z)
 
     # Concatenate variable y with string " is good"
+    y + " is good"
 
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
+    m = "awesome"
+    n = "good"
 
     return x, y, z, length, m, n
 
@@ -34,21 +39,29 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
+    list = n.split(" ")
 
     # Get all the items past the first of the third substring
+    list[1:]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
+    A = [[1, 4, 5],
+         [6, 10, 11],
+         [12, 17, 38]]        
 
     # Collect the items in the last column of matrix A using list comprehension
+    column = [row[2] for row in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
+    dia = [A[i][i] for i in [0, 1, 2] if A[i][i] % 2 ==0]
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
+    [ord(x) for x in "Stevens"]
 
     return p, r, c, d, o
 
@@ -61,23 +74,30 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
+    f = {"fruit":"apple", "quantity":4, "color":"green"}
 
     # Get the item in dictionary f that the key "fruit" maps to
+    d["fruit"]
 
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
+    d["quantity"] = 1 + d["quantity"]
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
+    d = {"name":{"first_name":"Grace", "last_name":"Hopper"}, "jobs":["scientist", "engineer"], "age":85}
 
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
+    d["jobs"].append("programmer")
 
     # Get the third job Grace has that you recently added
+    d["jobs"][2]
 
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
+    不懂啥意思？
 
     return a, f, p, k
 
