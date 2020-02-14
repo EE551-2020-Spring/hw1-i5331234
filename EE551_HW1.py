ca5d7813e23971d593fae24b30f96d69694c2a37
i5331234 <60754752+i5331234@users.noreply.github.com>
2020-02-13 21:22:46 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -17,17 +17,17 @@ def numbers_and_strings():
     y = "Stevens"
 
     # Repeat variable y 5 times
-    y * 5
+    z = y * 5
 
     # What is the length of z?
-    len(z)
+    length = len(z)
 
     # Concatenate variable y with string " is good"
-    n = y + " is good"
+    m = y + " is good"
 
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-    x = n.replace("good", "awesome")
-    x
+    n = m.replace("good", "awesome")
+    
 
     return x, y, z, length, m, n
 
@@ -39,10 +39,10 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-    list = n.split(" ")
+    p = n.split(" ")
 
     # Get all the items past the first of the third substring
-    list[1:]
+    r = p[2][1:]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
@@ -53,15 +53,15 @@ def lists():
          [12, 17, 38]]        
 
     # Collect the items in the last column of matrix A using list comprehension
-    column = [row[2] for row in A]
+    c = [row[2] for row in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-    dia = [A[i][i] for i in [0, 1, 2] if A[i][i] % 2 ==0]
+    d = [A[i][i] for i in [0, 1, 2] if A[i][i] % 2 ==0]
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-    [ord(x) for x in "Stevens"]
+    o = [ord(x) for x in "Stevens"]
 
     return p, r, c, d, o
 
@@ -77,30 +77,30 @@ def dictionaries():
     f = {"fruit":"apple", "quantity":4, "color":"green"}
 
     # Get the item in dictionary f that the key "fruit" maps to
-    d["fruit"]
+    a = f["fruit"]
 
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
-    d["quantity"] = 1 + d["quantity"]
+    f["quantity"] = 1 + f["quantity"]
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-    d = {"name":{"first_name":"Grace", "last_name":"Hopper"}, "jobs":["scientist", "engineer"], "age":85}
+    A = {"name":{"first_name":"Grace", "last_name":"Hopper"}, "jobs":["scientist", "engineer"], "age":85}
 
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-    d["jobs"].append("programmer")
+    A["jobs"].append("programmer")
 
     # Get the third job Grace has that you recently added
-    d["jobs"][2]
+    p = A["jobs"][2]
 
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-    y = "amazing_grace"
-    n = list(y)
-    n.sort()
-    n
+    target = "amazing_grace"
+    k = list(target)
+    k.sort()
+  
 
     return a, f, p, k
 
